18353c6a9c3bfac68c5f0c8962096442b5ad0854
Sravan kumar Reddy S <1017598@questgedc.com>
2019-06-05 18:47:11 +0530

    project renaming changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Ngifelse
+# Angularpractice
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.9.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ngifelse!');
+    expect(page.getTitleText()).toEqual('Welcome to angularpractice!');
   });
 
   afterEach(async () => {
