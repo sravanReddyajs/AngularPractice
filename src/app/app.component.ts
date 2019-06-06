2df1f5c68054a56b8d5de3f77ab4499af423cecc
Sravan kumar Reddy S <1017598@questgedc.com>
2019-06-06 14:49:25 +0530

    added ngIfelse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularpractice';
+  inputNumber : number;
+  isPrimeNumber : boolean ;
+  showResult:boolean;
+  constructor(){
+
+  }
+  checkPrimeNumberOrNot(){
+    let flag:number = 0;
+    this.showResult =true;
+    for(let i=2; i<= this.inputNumber ;i++){
+      if(this.inputNumber % i === 0){
+        flag++;
+      }
+    }
+    if(flag === 1){
+      this.isPrimeNumber = true;
+    }else{
+      this.isPrimeNumber = false;
+    }
+
+  }
+
+
+  disableResults(){
+    this.showResult = false;
+  }
 }
